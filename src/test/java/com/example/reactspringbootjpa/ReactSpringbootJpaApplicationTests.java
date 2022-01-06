c4e39e4feb61053304b042e81b4396d8bc33c0a6
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-06 23:31:42 +0900

    first commit

---
+++ src/test/java/com/example/reactspringbootjpa/ReactSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.reactspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
