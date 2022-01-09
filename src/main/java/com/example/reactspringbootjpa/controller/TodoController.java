8314c06fe0012cc0c7119e165f520b21d15d04c0
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-10 00:56:45 +0900

    서비스 레이어 : 비즈니스 로직

---
+++ src/main/java/com/example/reactspringbootjpa/controller/TodoController.java
@@ -0,0 +1,30 @@
+package com.example.reactspringbootjpa.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.reactspringbootjpa.dto.ResponseDTO;
+import com.example.reactspringbootjpa.service.TodoService;
+
+@RestController
+@RequestMapping("todo")
+public class TodoController {
+
+	@Autowired
+	private TodoService service;
+	
+	@GetMapping("/test")
+	public ResponseEntity<?> testTodo() {
+		String str = service.testService(); // 테스트 서비스 사용
+		List<String> list = new ArrayList<>();
+		list.add(str);
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return ResponseEntity.ok().body(response);
+	}
+}
---
+++ src/main/java/com/example/reactspringbootjpa/service/TodoService.java
@@ -0,0 +1,11 @@
+package com.example.reactspringbootjpa.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TodoService {
+
+	public String testService() {
+		return "Test Servcie";
+	}
+}
