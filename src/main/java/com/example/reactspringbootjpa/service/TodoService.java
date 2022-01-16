e95962fb5d8b9de5b0df75810b408eb9f1084f6f
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-17 00:27:27 +0900

    Service : create() Method

--- src/main/java/com/example/reactspringbootjpa/service/TodoService.java
+++ src/main/java/com/example/reactspringbootjpa/service/TodoService.java
@@ -1,11 +1,16 @@
 package com.example.reactspringbootjpa.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.reactspringbootjpa.model.TodoEntity;
 import com.example.reactspringbootjpa.persistence.TodoRepository;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class TodoService {
 
@@ -20,5 +25,23 @@ public class TodoService {
 		// TodoEntity 검색
 		TodoEntity savedEntity = repository.findByUserId(entity.getUserId()).get(0);
 		return "title: " + savedEntity.getTitle() + ", userId: " + savedEntity.getUserId();
+	}
+	
+	public List<TodoEntity> create(final TodoEntity entity) {
+		// validations
+		if (entity == null) {
+			log.warn("Entity cannot be null.");
+			throw new RuntimeException("Entity cannot be null.");
+		}
+		if (entity.getUserId() == null) {
+			log.warn("Unknown user.");
+			throw new RuntimeException("Unknown user.");
+		}
+		
+		repository.save(entity);
+		
+		log.info("Entity Id : {} is saved", entity.getId());
+		
+		return repository.findByUserId(entity.getUserId());
 	}
 }
