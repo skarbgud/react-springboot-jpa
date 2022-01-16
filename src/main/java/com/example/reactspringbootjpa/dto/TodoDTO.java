a526db396c4c8e0f3cf89dc23ede2084bf481bf8
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-17 01:05:26 +0900

    feature: Todo create() 백엔드 구현

--- src/main/java/com/example/reactspringbootjpa/dto/TodoDTO.java
+++ src/main/java/com/example/reactspringbootjpa/dto/TodoDTO.java
@@ -23,7 +23,6 @@ import lombok.NoArgsConstructor;
  	이런 경우 DTO에 에러 메세지 필드를 선언하고 DTO에 포함하면 된다.
  */
 
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
@@ -37,5 +36,13 @@ public class TodoDTO {
 		this.id = entity.getId();
 		this.title = entity.getTitle();
 		this.done = entity.isDone();
+	}
+	
+	public static TodoEntity toEntity(final TodoDTO dto) {
+		return TodoEntity.builder()
+							.id(dto.getId())
+							.title(dto.getTitle())
+							.done(dto.isDone())
+							.build();
 	}
 }
