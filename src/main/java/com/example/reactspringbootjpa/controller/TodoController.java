029ac213eca620c1d693862bd8addb9ca19777fc
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-18 22:45:16 +0900

    feature: Todo delete() 백엔드 구현

--- src/main/java/com/example/reactspringbootjpa/controller/TodoController.java
+++ src/main/java/com/example/reactspringbootjpa/controller/TodoController.java
@@ -6,11 +6,13 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.reactspringbootjpa.dto.ResponseDTO;
@@ -106,5 +108,36 @@ public class TodoController {
 		
 		// (6) ReponseDTO를 리턴한다.
 		return ResponseEntity.ok().body(response);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<?> delete(@RequestBody TodoDTO dto) {
+		try {
+			String temporaryUserId = "temporary-user"; // temporary user id.
+			
+			// (1) TodoEntity로 변환한다.
+			TodoEntity entity = TodoDTO.toEntity(dto);
+			
+			// (2) 임시 사용자 아이디를 설정
+			entity.setUserId(temporaryUserId);
+			
+			// (3) 서비스를 이용해 entity를 삭제한다.
+			List<TodoEntity> entities = service.delete(entity);
+			
+			// (4) 자바 스트림을 이용해 리턴된 엔티티 리스트를 TodoDTO 리스트로 변환한다.
+			List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+			
+			
+			// (5) 변환된 TodoDTO 리스트를 이용해 ResponseDTO를 초기화한다.
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+			// (6) ResponseDTO를 리턴한다.
+			return ResponseEntity.ok().body(response);
+		} catch (Exception e) {
+			// (7) 혹시 예외가 있는 경우 dto 대신 error에 메세지를 넣어 리턴한다.
+			String error = e.getMessage();
+			ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+			return ResponseEntity.badRequest().body(response);
+		}
 	}
 }
