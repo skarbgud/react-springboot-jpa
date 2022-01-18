d7e23e3e6bba4aed7d99deffc2d0f92c28558f11
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-18 22:25:44 +0900

    feature: Todo update() 백엔드 구현

--- src/main/java/com/example/reactspringbootjpa/service/TodoService.java
+++ src/main/java/com/example/reactspringbootjpa/service/TodoService.java
@@ -1,6 +1,7 @@
 package com.example.reactspringbootjpa.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -40,6 +41,26 @@ public class TodoService {
 	
 	public List<TodoEntity> retrieve(final String userId) {
 		return repository.findByUserId(userId);
+	}
+	
+	public List<TodoEntity> update(final TodoEntity entity) {
+		// (1) 저장할 엔티티가 유효한지 확인한다.
+		validate(entity);
+		
+		// (2) 넘겨받은 엔티티 id를 이용해 TodoEntity를 가져온다. (존재하지 않는 엔티티는 업데이트 할 수 없기 때문)
+		final Optional<TodoEntity> original = repository.findById(entity.getId());
+		
+		original.ifPresent(todo -> {
+			// (3) 반환된 TodoEntity가 존재하면 값을 새 entity 값으로 덮어 씌운다.
+			todo.setTitle(entity.getTitle());
+			todo.setDone(entity.isDone());
+			
+			// (4) 데이터베이스에 새 값을 저장한다.
+			repository.save(todo);
+		});
+		
+		// 모든 Todo 리스트를 반환한다.
+		return retrieve(entity.getUserId());
 	}
 	
 	private void validate(final TodoEntity entity) {
