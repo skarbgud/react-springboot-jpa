9678f6f8f2710740467a1811376a0a57ff20f3e2
GyuHyeong Nam <sjarbgud@gmail.com>
2022-01-24 02:15:29 +0900

    [FE] TODO 추가

--- react-workspace/todo-react-app/src/App.js
+++ react-workspace/todo-react-app/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Todo from './Todo';
-import { Paper, List } from '@material-ui/core';
+import { Paper, List, Container } from '@material-ui/core';
 import './App.css';
+import AddTodo from './AddTodo';
 
 class App extends React.Component {
   constructor(props) {
@@ -12,6 +13,16 @@ class App extends React.Component {
         { id: 1, title: 'Hello World 2', done: false },
       ],
     };
+  }
+
+  // (1) 함수 추가
+  add = (item) => {
+    const thisItems = this.state.items;
+    item.id = "ID-" + thisItems.length; // KEY를 위한 ID추가
+    item.done = false // done 초기화
+    thisItems.push(item); // 리스트에 아이템 추가
+    this.setState({ items: thisItems }); // 업데이트는 반드시 this.setState로 해야 됨
+    console.log("items: ", this.state.items);
   }
 
   render() {
@@ -25,7 +36,14 @@ class App extends React.Component {
       </Paper>
     );
 
-    return <div className="App">{todoItems}</div>;
+    return (
+      <div className="App">
+        <Container maxWidth="md">
+          <AddTodo add={this.add}/>
+          <div className="TodoList">{todoItems}</div>
+        </Container>
+      </div>
+    );
   }
 }
 
