3b6891e29783fdd066790ad91525584abcba567b
GyuHyeong Nam <sjarbgud@gmail.com>
2022-02-04 03:20:45 +0900

    [FE] 로컬 스토리지에 ACCESS TOKEN 저장

--- react-workspace/todo-react-app/src/service/ApiService.js
+++ react-workspace/todo-react-app/src/service/ApiService.js
@@ -1,13 +1,22 @@
 import { API_BASE_URL } from "../api-config";
+const ACCESS_TOKEN = "ACCESS_TOKEN";
 
 export function call(api, method, request) {
-  let options = {
-    headers: new Headers({
+  let headers = new Headers({
       "Content-Type": "application/json",
-    }),
+  });
+  // 로컬 스토리지에서 ACCESS_TOKEN 가져오기
+  const accessToken = localStorage.getItem(ACCESS_TOKEN);
+  if(accessToken && accessToken != null) {
+    headers.append("Authorization", "Bearer " + accessToken);
+  }
+
+  let options = {
+    headers: headers,
     url: `${API_BASE_URL}${api}`,
     method: method,
   };
+
   if (request) {
     // GET method
     options.body = JSON.stringify(request);
@@ -22,6 +31,7 @@ export function call(api, method, request) {
       return json;
     })
   )
+  
   .catch((error) => {
     if (error.status === 403) {
       window.location.href = "/login"; // 접근권한 없을경우 redirect
@@ -33,8 +43,9 @@ export function call(api, method, request) {
 export function signin(userDTO) {
   return call("/auth/signin", "POST", userDTO) 
     .then((response) => {
-      // console.log(`response: ${response}`);
-      // alert(`로그인 토큰: ${response.token}`);
+      // 로컬 스토리지에 토큰 저장
+      localStorage.setItem(ACCESS_TOKEN, response.token);
+      // token이 존재하면 /로 리다이렉트
       window.location.href = "/";
     })
 }
