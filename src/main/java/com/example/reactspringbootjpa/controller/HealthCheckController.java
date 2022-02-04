652fd833b9103ac2591a82f9fe11d624c0d56b50
GyuHyeong Nam <sjarbgud@gmail.com>
2022-02-04 19:16:30 +0900

    패키지 구조 변경 및 AWS Elastic Beanstalk 배포

---
+++ src/main/java/com/example/reactspringbootjpa/controller/HealthCheckController.java
@@ -0,0 +1,14 @@
+package com.example.reactspringbootjpa.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/")
+    public String healthCheck() {
+        return "The Service is up and runing....";
+    }
+
+}
