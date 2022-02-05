f4710b71e594f539b2561e391eb3a71711315759
GyuHyeong Nam <sjarbgud@gmail.com>
2022-02-05 18:51:02 +0900

    FE DNS 수정(재수정)

--- frontend/src/api-config.js
+++ frontend/src/api-config.js
@@ -6,7 +6,7 @@ const hostname = window && window.location && window.location.hostname;
 if (hostname === 'localhost') {
   backendHost = 'http://localhost:8080';
 } else {
-  backendHost = 'https://www.reactbootjpa.tk/';
+  backendHost = 'https://www.reactbootjpa.tk';
 }
 
 export const API_BASE_URL = `${backendHost}`;
